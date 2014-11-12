760fde299cde2ef68fa108a047a1a7b07cb0ea8e
rev <rev@rev-VirtualBox.(none)>
2014-11-11 22:23:51 -0600

    styling and logo changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,6 +18,9 @@ angular.module('onionApp', [
       .when('/search', {
         templateUrl: 'views/search.html'
       })
+      .when('/signup-completed', {
+        templateUrl: 'views/signup-completed.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
